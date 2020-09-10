6e83e79106aabd4726e2db2c5106343986726902
Jossean Rivera <jossean.rivera@outlook.com>
2020-09-10 15:24:03 -0400

    Adding ability to book an appointment

--- BrainHi.WebApp/Controllers/ProvidersController.cs
+++ BrainHi.WebApp/Controllers/ProvidersController.cs
@@ -1,6 +1,8 @@
 ﻿using BrainHi.Data;
 using BrainHi.Models;
+using BrainHi.WebApp.ViewModels.Providers;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -45,10 +47,66 @@ namespace BrainHi.WebApp.Controllers
             if (!id.HasValue) return View("NotFound");
 
             //  Get provider from repository
-            Provider provider = await _repository.GetEntityAsync<Provider>(id.Value);
+            Provider provider = await _repository.GetQueryBuilder<Provider>()
+                .Include(p => p.Appointments)
+                .FirstOrDefaultAsync(p => p.ProviderId == id.Value);
 
             //  Return view result
-            return View(provider);
+            return View(new ProviderDisplayViewModel { Provider = provider });
+        }
+
+        //  Post action to save an appointment
+        [HttpPost("book-appointment")]
+        public async Task<IActionResult> Book(ProviderDisplayViewModel inputModel)
+        {
+            //  Query provider
+            Provider provider = await _repository.GetQueryBuilder<Provider>()
+                .Include(p => p.Appointments)
+                .FirstOrDefaultAsync(p => p.ProviderId == inputModel.ProviderId);
+
+            //  The provider is required
+            if (provider == null) return View("NotFound");
+
+            //  Display any errors
+            if (!ModelState.IsValid) return View(nameof(Display), new ProviderDisplayViewModel { Provider = provider });
+
+            //  Save appointment (assuming that the end datetime is 1 hour after the selected date)
+            Appointment appointment = await _repository.AddEntityAsync(new Appointment
+            {
+                ProviderId = inputModel.ProviderId,
+                AppointmentReason = inputModel.AppointmentReason,
+                EndTime = new DateTime(inputModel.Year.Value, inputModel.Month.Value, inputModel.Day.Value, inputModel.Hour.Value + 1, inputModel.Minute.Value, 0),
+                StartTime = new DateTime(inputModel.Year.Value, inputModel.Month.Value, inputModel.Day.Value, inputModel.Hour.Value, inputModel.Minute.Value, 0),
+                PatientDoB = new DateTime(inputModel.PatientDoBYear.Value, inputModel.PatientDoBMonth.Value, inputModel.PatientDoBDay.Value),
+                PatientFullName = inputModel.PatientFullName,
+                PatientGender = inputModel.PatientGender,
+                PatientPhoneNumber = inputModel.PatientPhoneNumber
+            });
+
+            //  Temporarily save the Id of appointment
+            TempData[nameof(Appointment.AppointmentId)] = appointment.AppointmentId;
+
+            //  Redirect to list page
+            return RedirectToAction(nameof(BookConfirm));
+        }
+
+        //  Displays page with successful message about the appointment
+        [HttpGet("appointment-confirm")]
+        public async Task<ViewResult> BookConfirm()
+        {
+            //  Get Id of new appointment
+            int? appointmentId = TempData[nameof(Appointment.AppointmentId)] as int?;
+
+            //  ID is required
+            if (!appointmentId.HasValue) return View("NotFound");
+
+            //  Query appointment
+            Appointment appointment = await _repository.GetQueryBuilder<Appointment>()
+                .Include(ap => ap.Provider)
+                .FirstOrDefaultAsync(ap => ap.AppointmentId == appointmentId.Value);
+
+            //  Return view result to display view about the confirmation of the appointment
+            return View(appointment);
         }
 
         //  Displays form to create a new provider
--- BrainHi.WebApp/ViewModels/Providers/ProviderDisplayViewModel.cs
+++ BrainHi.WebApp/ViewModels/Providers/ProviderDisplayViewModel.cs
@@ -1,4 +1,7 @@
 ﻿using BrainHi.Models;
+using System;
+using System.ComponentModel.DataAnnotations;
+using System.Text.Json.Serialization;
 
 namespace BrainHi.WebApp.ViewModels.Providers
 {
@@ -11,5 +14,108 @@ namespace BrainHi.WebApp.ViewModels.Providers
         /// Provider data to display
         /// </summary>
         public Provider Provider { get; set; }
+
+        /// <summary>
+        /// Unique identifier of the Provider associated with this Appointment
+        /// </summary>
+        [Required]
+        [JsonPropertyName("provider_id")]
+        [Display(Name = "Provider ID")]
+        public int ProviderId { get; set; }
+
+        /// <summary>
+        /// Number of the month for the appointment
+        /// </summary>
+        [Required]
+        [Range(1, 12)]
+        public int? Month { get; set; }
+
+        /// <summary>
+        /// Number of the day for the appointment date
+        /// </summary>
+        [Required]
+        [Range(1, 31)]
+        public int? Day { get; set; }
+
+        /// <summary>
+        /// Number of year for the appointment date
+        /// </summary>
+        [Required]
+        [Range(2020, 2099)]
+        public int? Year { get; set; }
+
+        /// <summary>
+        /// Number of hour for the appointment time
+        /// </summary>
+        [Required]
+        [Range(1, 24)]
+        public int? Hour { get; set; }
+
+        /// <summary>
+        /// Number of minute for the appointment time
+        /// </summary>
+        [Required]
+        [Range(0, 59)]
+        public int? Minute { get; set; }
+
+        /// <summary>
+        /// Text describing why the patient booked and Appointment
+        /// </summary>
+        [Required]
+        [Display(Name = "Reason")]
+        [JsonPropertyName("appointment_reason")]
+        [StringLength(4000)]
+        public string AppointmentReason { get; set; }
+
+        /// <summary>
+        /// Full name of the Patient that requested the Appointment
+        /// </summary>
+        [Required]
+        [Display(Name = "Patient Full Name")]
+        [JsonPropertyName("patient_full_name")]
+        [StringLength(128)]
+        public string PatientFullName { get; set; }
+
+        /// <summary>
+        /// Gender of the Patient that requested the Appointment
+        /// </summary>
+        [Required]
+        [Display(Name = "Patient Gender")]
+        [JsonPropertyName("patient_gender")]
+        public PatientGender? PatientGender { get; set; }
+
+        /// <summary>
+        /// Number of the month for the patient's date of birth
+        /// </summary>
+        [Required]
+        [Range(1, 12)]
+        [Display(Name = "Month")]
+        public int? PatientDoBMonth { get; set; }
+
+        /// <summary>
+        /// Number of the day for the patient's date of birth
+        /// </summary>
+        [Required]
+        [Range(1, 31)]
+        [Display(Name = "Day")]
+        public int? PatientDoBDay { get; set; }
+
+        /// <summary>
+        /// Number of year for the patient's date of birth
+        /// </summary>
+        [Required]
+        [Range(1900, 2020)]
+        [Display(Name = "Year")]
+        public int? PatientDoBYear { get; set; }
+
+        /// <summary>
+        /// Phone number of the Patient that requested the Appointment
+        /// </summary>
+        [Required]
+        [Phone]
+        [StringLength(16)]
+        [Display(Name = "Patient Phone Number")]
+        [JsonPropertyName("patient_phone_number")]
+        public string PatientPhoneNumber { get; set; }
     }
 }
